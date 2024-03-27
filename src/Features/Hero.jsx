e268c8b6827db4cc5c6a81ecc1c3bd57e6c94a3b
Dandrizzy <dandrizzy4all@gmail.com>
2024-03-27 10:07:29 +0100

    update

--- src/Features/Hero.jsx
+++ src/Features/Hero.jsx
@@ -128,7 +128,7 @@ export default function Hero() {
                   <Form onSubmit={handleSubmit(submit)}>
                     <div className="relative mt-2 rounded-md shadow-sm">
                       <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                        <span className="text-gray-500 sm:text-sm"><BiMapPin /></span>
+                        <span className="text-gray-500 sm:text-sm"><BiMapPin className=' text-2xl ' /></span>
                       </div>
                       <input
                         required
@@ -136,7 +136,7 @@ export default function Hero() {
                           required: 'This field is required'
                         })}
                         id="trackingCode"
-                        className="block w-[30rem] px-10 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        className="block w-[28rem] px-10 rounded-md text-2xl border-0 py-1.5 pl-10 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         placeholder="Tracking code"
                       />
                       <div className="absolute inset-y-0 right-0 flex items-center">
@@ -145,6 +145,8 @@ export default function Hero() {
                         </label>
                         <Button
                           type='submit'
+                          size='3'
+                          color='violet'
                           className="h-full border-0 outline-none bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
                         >
                           Track
