4ebd99729f96d7985f2e1f9092fe56854373e7f2
Dandrizzy <dandrizzy4all@gmail.com>
2024-03-27 10:10:13 +0100

    update

--- src/Features/Hero.jsx
+++ src/Features/Hero.jsx
@@ -136,7 +136,7 @@ export default function Hero() {
                           required: 'This field is required'
                         })}
                         id="trackingCode"
-                        className="block w-[28rem] px-10 rounded-md text-2xl border-0 py-1.5 pl-10 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        className="block w-[24rem] md:w-[30rem] px-10 rounded-md text-2xl border-0 py-1.5 pl-10 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         placeholder="Tracking code"
                       />
                       <div className="absolute inset-y-0 right-0 flex items-center">
